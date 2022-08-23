c02375b87011e67bed2ea96bc6f8246336cd2102
vader9595 <102444679+vader9595@users.noreply.github.com>
2022-08-23 11:14:20 -0500

    Initial commit

---
+++ src/main/java/com/example/SpringBoot_403/SpringBoot403Application.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot_403;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot403Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot403Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBoot_403/SpringBoot403ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot_403;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot403ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
